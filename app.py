723977b38c74a9d0db16fe6a5cf155ffb0d20fc2
nia-coder3101 <nishchithaadi2007@gmail.com>
2026-05-08 22:18:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import hashlib
 # ==========================================
 API_KEY = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash-latest')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 from PIL import Image
 import os
