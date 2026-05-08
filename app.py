fce8a2a9eb623ded117858c4f10abb390e81ebd7
nia-coder3101 <nishchithaadi2007@gmail.com>
2026-05-08 20:50:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ import hashlib
 # ==========================================
 # 1. THE BRAIN (Backend Setup)
 # ==========================================
-API_KEY = "AIzaSyDIZv6Wte1eUuQp9jHXft3GC1Wk91wFpAc"
+API_KEY = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-2.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 from PIL import Image
 import os
